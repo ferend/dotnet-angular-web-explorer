26ff63fa8a3b35b3857a51af15b5c69c4e8b843b
Eren Dalçık <dalcikferhateren@gmail.com>
2024-01-02 17:16:48 +0300

    updates

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -1,31 +1,29 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
-import { RouterModule } from '@angular/router';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { HttpClientModule, HTTP_INTERCEPTORS } from "@angular/common/http";
+import { RouterModule } from "@angular/router";
 
-import { AppComponent } from './app.component';
-import { NavMenuComponent } from './nav-menu/nav-menu.component';
-import { HomeComponent } from './home/home.component';
-import { CounterComponent } from './counter/counter.component';
-import { FetchDataComponent } from './fetch-data/fetch-data.component';
+import { AppComponent } from "./app.component";
+import { NavMenuComponent } from "./nav-menu/nav-menu.component";
+import { HomeComponent } from "./home/home.component";
+
+import { MatCardModule } from "@angular/material/card";
+import { MatInputModule } from "@angular/material/input";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    NavMenuComponent,
-    HomeComponent,
-    CounterComponent,
-    FetchDataComponent
-  ],
+  declarations: [AppComponent, NavMenuComponent, HomeComponent],
   imports: [
-    BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
+    BrowserModule.withServerTransition({ appId: "ng-cli-universal" }),
     HttpClientModule,
     FormsModule,
+    MatCardModule,
+    MatInputModule,
+    BrowserAnimationsModule,
+
     RouterModule.forRoot([
-      { path: '', component: HomeComponent, pathMatch: 'full' },
-      { path: 'counter', component: CounterComponent },
-      { path: 'fetch-data', component: FetchDataComponent },
+      { path: "", component: HomeComponent, pathMatch: "full" },
     ])
   ],
   providers: [],
---
+++ Infrastructure/HackerNewsRepository.cs
@@ -0,0 +1,26 @@
+﻿using WebExplorer.Core.Interfaces;
+
+namespace WebExplorer.Infrastructure
+{
+
+    public class HackerNewsRepository : IHackerNewsRepository
+    {
+
+        private static HttpClient client = new HttpClient();
+
+        public HackerNewsRepository()
+        {
+
+        }
+
+        public async Task<HttpResponseMessage> BestStoriesAsync()
+        {
+            return await client.GetAsync("https://hacker-news.firebaseio.com/v0/beststories.json");
+        }
+
+        public async Task<HttpResponseMessage> GetStoryByIdAsync(int id)
+        {
+            return await client.GetAsync(string.Format("https://hacker-news.firebaseio.com/v0/item/{0}.json", id));
+        }
+    }
+}
